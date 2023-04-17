1a44f612cf6707f708c1926a2056c445988dc078
joelquintel <joel.quintel@hotmail.com>
2023-04-17 11:43:31 -0300

    campo uf adicionado

--- Formulario/exemplo2.py
+++ Formulario/exemplo2.py
@@ -48,44 +48,54 @@ class Pessoas:
 
 listaCadastro = {}
 
-window = Tk()
-window.title("Cadastro")
-window.geometry('600x400')
-lbl = Label(window, text="Nome")
-lbl.grid(column=0, row=0)
-txtNome = Entry(window,width=10)
-txtNome.grid(column=1, row=0)
-#Telefone
-lbl2 = Label(window, text="Telefone")
-lbl2.grid(column=5, row=0)
-txtTel = Entry(window,width=10)
-txtTel.grid(column=7, row=0)
 
-def clicked():
-
-    nomeCd =  txtNome.get()
-    telefoneCd =  txtTel.get()
-
-    listaCadastro.update({"nome":nomeCd,"telefone":telefoneCd})
-
-btn = Button(window, text="Cadastrar", command=clicked)
-
-btn.grid(column=2, row=7)
-
-window.mainloop()    
 
 if __name__ == "__main__":
 
     try:
+        window = Tk()
+        window.title("Cadastro")
+        window.geometry('600x400')
+        #Nome
+        lbl = Label(window, text="Nome:")
+        lbl.grid(column=0, row=0)
+        txtNome = Entry(window,width=10)
+        txtNome.grid(column=1, row=0)
+        #Telefone
+        lbl2 = Label(window, text="Telefone:")
+        lbl2.grid(column=5, row=0)
+        txtTel = Entry(window,width=10)
+        txtTel.grid(column=7, row=0)
+        #UF
+        lbl3 = Label(window, text="UF:")
+        lbl3.grid(column=21, row=0)
+        txtUF = Entry(window,width=10)
+        txtUF.grid(column=28, row=0)
+
+        def clicked():
+            #pega os valores inserido no input do formulario 
+            nomeCd =  txtNome.get()
+            telefoneCd =  txtTel.get()
+            ufCd = txtUF.get()
+            #inserindo novos dados no lista de dicionarios
+            listaCadastro.update({"nome":nomeCd,"telefone":telefoneCd,"uf":ufCd})
+            #salvando no arquivo json
+            pessoas = Pessoas(os_path.abspath("./pessoas.json"), 'UTF-8')
+            pessoas.create_empty_json()
+            pessoas.write_json(listaCadastro)
+            print(pessoas.get_documents())
+
+        btn = Button(window, text="Cadastrar", command=clicked)
+
+        btn.grid(column=15, row=25)
+
+      
         
 
        # telefones = [{"nome": "Isabela Simone Corte Real",
                    # "telefone": "(94) 98550-6067", "estado": "PA"}]
         
-        pessoas = Pessoas(os_path.abspath("./pessoas.json"), 'UTF-8')
-        pessoas.create_empty_json()
-        pessoas.write_json(listaCadastro)
-        print(pessoas.get_documents())
-
+      
+        window.mainloop()     
     except Exception as e:
         print("Erro:>>>>>>>>>>>>>>", e)
