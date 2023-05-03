c9afe7ab47411820d61355d6d4338da7a312da9a
rodrigmars <rodrigmars@gmail.com>
2023-05-03 17:10:05 -0300

    Alias ajustado na importação do tkinter

--- Formulario/main.py
+++ Formulario/main.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 import os
 from PIL import ImageTk, Image  
 from view.menu import Menu
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     try:
         pastaImg= os.path.dirname(__file__)
-        menu = Tk()
+        menu = tk.Tk()
         menu.title("CRUD")
         width= menu.winfo_screenwidth()
         height= menu.winfo_screenheight()
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
         image1 = Image.open("Formulario/view/img/bg_menu.png").resize((1367,720))
         imgBG = ImageTk.PhotoImage(image1)
-        labelBG = Label(menu,image = imgBG)
+        labelBG = tk.Label(menu,image = imgBG)
         labelBG.pack()
         Menu(menu)
     
