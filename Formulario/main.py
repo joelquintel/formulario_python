b3b3e82eed9ca3c5ccd3ee14a30446209e0f2851
rodrigmars <rodrigmars@gmail.com>
2023-05-04 12:53:19 -0300

    Ajustes executados para config e main

--- Formulario/main.py
+++ Formulario/main.py
@@ -1,28 +1,39 @@
 import tkinter as tk
 import os
-from PIL import ImageTk, Image  
+from PIL import ImageTk, Image
 from view.menu import Menu
+
+from config import load_toml
+
+def main(configs: dict):
+
+    menu = tk.Tk()
+    
+    menu.title("CRUD")
+    
+    width = menu.winfo_screenwidth()
+    height = menu.winfo_screenheight()
+    
+    menu.geometry("%dx%d" % (width, height))
+    menu.resizable(False, False)
+
+    image1 = Image.open(configs["bg_menu"]).resize((1367, 720))
+    imgBG = ImageTk.PhotoImage(image1)
+    labelBG = tk.Label(menu, image=imgBG)
+    
+    labelBG.pack()
+
+    Menu(menu)
+    
+    menu.mainloop()
+
+
 if __name__ == "__main__":
 
     try:
-        pastaImg= os.path.dirname(__file__)
-        menu = tk.Tk()
-        menu.title("CRUD")
-        width= menu.winfo_screenwidth()
-        height= menu.winfo_screenheight()
-        menu.geometry("%dx%d" % (width, height))
-        menu.resizable(False,False)
+        configs = load_toml()
 
-        image1 = Image.open("Formulario/view/img/bg_menu.png").resize((1367,720))
-        imgBG = ImageTk.PhotoImage(image1)
-        labelBG = tk.Label(menu,image = imgBG)
-        labelBG.pack()
-        Menu(menu)
-    
-       # telefones = [{"nome": "Isabela Simone Corte Real",
-                   # "telefone": "(94) 98550-6067", "estado": "PA"}]
-        
-        menu.mainloop()
-        #window.mainloop()     
     except Exception as e:
         print("Erro:>>>>>>>>>>>>>>", e)
+    else:
+        main(configs)
